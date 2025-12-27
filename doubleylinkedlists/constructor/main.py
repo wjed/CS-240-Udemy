bb82df1796afd405a303b8f8c8b3b195f763a3cd
Will Jedrzejczak <williamjedrzejczak@outlook.com>
2025-12-27 13:37:31 -0500

    DLL Constructor

---
+++ doubleylinkedlists/constructor/main.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+
+my_doubly_linked_list = DoublyLinkedList(7)
