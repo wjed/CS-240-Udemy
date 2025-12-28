b68c11671751b8ea7c341bf494c9a5f65f224fbc
Will Jedrzejczak <williamjedrzejczak@outlook.com>
2025-12-28 13:47:48 -0500

    S + Q

---
+++ stacksandqueues/queue/alloperations/main.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+    def print_queue(self):
+        temp = self.first
+        while temp:
+            print(temp.value)
+            temp = temp.next
+    def enque(self, value):
+        new_node = Node(value)
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+    def deque(self):
+        if self.length == 0:
+            return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            temp.next = None
+        self.length -= 1
+
+            
+
+my_queue = Queue(1)
+my_queue.enque(2)
+my_queue.enque(3)
+my_queue.deque()
+my_queue.print_queue()
