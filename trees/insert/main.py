41e942e37a7d28a12278f79044481c172632834a
Will Jedrzejczak <williamjedrzejczak@outlook.com>
2025-12-31 14:59:05 -0500

    BST

--- trees/insert/main.py
+++ trees/insert/main.py
@@ -21,17 +21,35 @@ class BinarySearchTree:
                 if temp.left is None:
                     temp.left = new_node
                     return True
-                else:
-                    temp = temp.left
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+    def contains(self, value):
+        if self.root == None:
+            return False
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
+
+            
         
 
 
 my_tree = BinarySearchTree()
-my_tree.insert(1)
 my_tree.insert(2)
+my_tree.insert(1)
 my_tree.insert(3)
-my_tree.insert(4)
-my_tree.insert(5)
-my_tree.insert(6)
-my_tree.insert(7)
-print(my_tree.root)
+print(my_tree.root.value)
+print(my_tree.root.left.value)
+print(my_tree.root.right.value)
+print(my_tree.contains(3))
+
