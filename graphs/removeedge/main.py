74f120d5f3f6fb2d9069194364697d61260c96f5
Will Jedrzejczak <williamjedrzejczak@outlook.com>
2026-01-02 17:16:24 -0500

    Remove Edge

--- graphs/removeedge/main.py
+++ graphs/removeedge/main.py
@@ -7,33 +7,38 @@ class Graph:
             print(vertex, ':', self.adj_list[vertex])
 
     def add_vertex(self, vertex):
-        self.adj_list[vertex] = []
-        if vertex not in self.adj_list.keys():
+        if vertex not in self.adj_list:
             self.adj_list[vertex] = []
             return True
         return False
     
     def add_edge(self, v1, v2):
-        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
-            self.adj_list[v1].append(v2)
-            self.adj_list[v2].append(v1)
+        if v1 in self.adj_list and v2 in self.adj_list:
+            if v2 not in self.adj_list[v1]:
+                self.adj_list[v1].append(v2)
+                self.adj_list[v2].append(v1)
             return True
         return False
     
-    def remove_edge(self):
-        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
-            self.adj_list[v1].remove(v2)
-            self.adj_list[v2].remove(v1)
-            return True
+    def remove_edge(self, v1, v2):
+        if v1 in self.adj_list and v2 in self.adj_list:
+            if v2 in self.adj_list[v1]:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+                return True
         return False
-    
-    
-
 
 my_graph = Graph()
 
-my_graph.add_vertex(1)
-my_graph.add_vertex(2)
-my_graph.add_edge(1,2)
+my_graph.add_vertex('A')
+my_graph.add_vertex('B')
+my_graph.add_vertex('C')
+my_graph.add_vertex('D')
+
+my_graph.add_edge('A', 'B')
+my_graph.add_edge('B', 'C')
+my_graph.add_edge('C', 'A')
+
+my_graph.remove_edge('A', 'D')
 
 my_graph.print_graph()
