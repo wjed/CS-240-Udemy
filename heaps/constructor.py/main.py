01fb9f7afb2199636f79eceec782073f5a8804bc
Will Jedrzejczak <williamjedrzejczak@outlook.com>
2026-01-04 15:54:28 -0500

    Completed insert method for heaps

--- heaps/constructor.py/main.py
+++ heaps/constructor.py/main.py
@@ -10,3 +10,8 @@ class MaxHeap:
     def _swap(self, index1, index2):
         self.head[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
     def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
